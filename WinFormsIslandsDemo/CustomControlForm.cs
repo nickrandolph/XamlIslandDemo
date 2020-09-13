72a743586d64d2d79284999abc9791e03792e975
Nick Randolph <nick@builttoroam.com>
2020-09-13 14:01:06 +1000

    Adding custom control

--- WinFormsIslandsDemo/CustomControlForm.cs
+++ WinFormsIslandsDemo/CustomControlForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using UwpControlLibrary;
 
 namespace WinFormsIslandsDemo
 {
@@ -13,6 +14,16 @@ namespace WinFormsIslandsDemo
         public CustomControlForm()
         {
             InitializeComponent();
+
+            var myHostControl = new Microsoft.Toolkit.Forms.UI.XamlHost.WindowsXamlHost();
+            myHostControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            myHostControl.Name = "hostUwpButton";
+            var uwpButton = Microsoft.Toolkit.Win32.UI.XamlHost.UWPTypeFactory.CreateXamlContentByType(
+                "UwpControlLibrary.MyCustomControl") as MyCustomControl;
+            uwpButton.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Stretch;
+            uwpButton.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Stretch;
+            myHostControl.Child = uwpButton;
+            this.Controls.Add(myHostControl);
         }
     }
 }
